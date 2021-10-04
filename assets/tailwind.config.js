94675b3e0ef58eeff9b8c35b1c74a3d9567d40a5
Michael Hanna <mykelkim@yahoo.com>
2021-10-04 12:34:21 -0400

    s3 upload OK. phx 1.6, otp 24.x

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "jit",
+    purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
